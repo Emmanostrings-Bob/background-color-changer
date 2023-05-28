ac3bb025440a2ed1c6a97348f999bf27b151c5d5
Emmanostrings-Bob <oluwafemiayomide852@gmail.com>
2023-05-28 03:28:35 +0100

    code_updated

---
+++ script.js
@@ -0,0 +1,73 @@
+// var index = 0;
+
+// function changeColors() {
+//     var colors = ["red", "blue", "orange", "yellow", "green", "purple"];
+
+//     document.getElementsByTagName("body")[0].style.background = colors[index++];
+
+//     if(index > colors.length - 1){
+//         index = 0;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let index = 0;
+
+function changeColors(){
+  let colors = ["blue", "red", "green", "violet", "pink", "coral", "purple"];
+
+    document.getElementsByTagName("body")[0].style.background = colors[index++];
+    
+    if(index > colors.length -1){
+        index = 0;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
