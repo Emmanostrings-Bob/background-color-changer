1e3a8dba2709f9fcd5ee488743d5fb7f81702388
Emmanostrings-Bob <oluwafemiayomide852@gmail.com>
2023-05-28 03:30:33 +0100

    Background_colorChanger

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
-// var index = 0;
+var index = 0;
 
-// function changeColors() {
-//     var colors = ["red", "blue", "orange", "yellow", "green", "purple"];
-
-//     document.getElementsByTagName("body")[0].style.background = colors[index++];
-
-//     if(index > colors.length - 1){
-//         index = 0;
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let index = 0;
-
-function changeColors(){
-  let colors = ["blue", "red", "green", "violet", "pink", "coral", "purple"];
+function changeColors() {
+    var colors = ["red", "blue", "orange", "yellow", "green", "purple"];
 
     document.getElementsByTagName("body")[0].style.background = colors[index++];
-    
-    if(index > colors.length -1){
+
+    if(index > colors.length - 1){
         index = 0;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let index = 0;
+
+// function changeColors(){
+//   let colors = ["blue", "red", "green", "violet", "pink", "coral", "purple"];
+
+//     document.getElementsByTagName("body")[0].style.background = colors[index++];
+    
+//     if(index > colors.length -1){
+//         index = 0;
+//     }
+// }
 
 
 
